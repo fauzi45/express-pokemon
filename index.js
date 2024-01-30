7fc2f06bc1a204c8933486c108a7c782378697e1
fauzi45 <uzi.fajar45@gmail.com>
2024-01-30 08:38:49 +0700

    init index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+const port = process.env.NODEJS_PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.send("Succesfull Response");
+});
+
+app.listen(port, console.log("start port"));
