b44a6135d84dace1eb6e3e0cae5fc5fa5d5c0a28
fauzi45 <uzi.fajar45@gmail.com>
2024-01-30 11:13:08 +0700

    feat: catch pokemon

--- server/api/pokemon.js
+++ server/api/pokemon.js
@@ -18,20 +18,33 @@ const allPokemon = async (request, reply) => {
 };
 
 const detailPokemon = async (req, res) => {
-    try {
-      const { id } = req.params;
-      const data = await PokemonHelper.getPokemonDetail(id);
-      res
-        .status(200)
-        .send({ message: "Data Pokemon Detail berhasil didapat", data });
-    } catch (err) {
-      console.error("Gagal mendapatkan data pokemon >>>>>", err.message);
-      res.status(400).send({ message: err.message });
-    }
-  };
-  
+  try {
+    const { id } = req.params;
+    const data = await PokemonHelper.getPokemonDetail(id);
+    res
+      .status(200)
+      .send({ message: "Data Pokemon Detail berhasil didapat", data });
+  } catch (err) {
+    console.error("Gagal mendapatkan data pokemon >>>>>", err.message);
+    res.status(400).send({ message: err.message });
+  }
+};
+
+const catchPokemon = async (req, res) => {
+  try {
+    const { name } = req.body;
+    const data = await PokemonHelper.catchPokemon(name);
+    res
+      .status(200)
+      .send({ message: "Pokemon berhasil ditangkap", data });
+  } catch (err) {
+    console.error("Gagal mendapatkan pokemon >>>>>", err.message);
+    res.status(400).send({ message: err.message });
+  }
+};
 
 Router.get("/all", allPokemon);
 Router.get("/:id", detailPokemon);
+Router.post("/catch", catchPokemon);
 
 module.exports = Router;
--- server/helpers/pokemonHelper.js
+++ server/helpers/pokemonHelper.js
@@ -4,8 +4,7 @@ const general = require("../helpers/generalHelper");
 const dataPath = (__dirname, "./assets/pokemon.json");
 
 const baseUrl = "https://pokeapi.co/api/v2/";
-
-
+const localUrl = (__dirname, "./assets/pokemon.json");
 const getPokemonList = async () => {
   try {
     const response = await general.commonHttpRequest({
@@ -28,20 +27,51 @@ const getPokemonDetail = async (id) => {
       baseURL: baseUrl,
       url: `pokemon/${id}`,
     });
-    const data = ({
+    const data = {
       id: response.id,
       name: response.name,
-    })
-    console.log(data);
+    };
     return Promise.resolve(data);
   } catch (error) {
     console.log("Gagal mendapatkan data pokemon");
-    throw error; 
+    throw error;
   }
 };
 
+const catchPokemon = async (name) => {
+  try {
+    const isCaught = Math.random() < 1;
+    if (isCaught) {
+      const dataLocal = fs.readFileSync(localUrl, "utf-8");
+      const jsonData = JSON.parse(dataLocal);
+
+      const response = await general.commonHttpRequest({
+        method: "get",
+        baseURL: baseUrl,
+        url: `pokemon/${name}`,
+      });
+
+      const data = {
+        id: jsonData.length + 1,
+        name: response.name,
+        nickname: response.name,
+      };
+      
+      jsonData.push(data);
+      fs.writeFileSync(localUrl, JSON.stringify(jsonData));
+      return Promise.resolve(data);
+    } else {
+      console.log("gagal");
+      throw new Error("Gagal mendapatkan pokemon");
+    }
+  } catch (error) {
+    console.log("Gagal mendapatkan pokemon");
+    throw error;
+  }
+};
 
 module.exports = {
   getPokemonList,
   getPokemonDetail,
+  catchPokemon,
 };
